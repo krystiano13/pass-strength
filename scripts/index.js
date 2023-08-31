bf98a27af49bb6162692c7f3e1c26297f03993b5
krystiano13 <crystianotv@gmail.com>
2023-08-31 11:13:46 +0200

    add painting of the strength bar

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,20 @@ const barElements = strengthBar.querySelectorAll('div');
 const input = document.querySelector('input');
 const specialCharacters = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
-
 let passwordStrength = 0;
 let password = input.value;
 let length = password.length;
 let upperCaseCount = 0;
 let specialCount = 1;
 
+const paintBar = (index,bounds) => {
+    if (passwordStrength >= bounds) {
+        barElements[index].style.backgroundColor = "var(--accent)";
+    }
+    else {
+        barElements[index].style.backgroundColor = "white";
+    }
+}
 
 const measureStrength = () => {
     password = input.value;
@@ -28,6 +35,9 @@ const measureStrength = () => {
     }
 
     passwordStrength = (password.length + (upperCaseCount * 1.25)) * specialCount;
+    paintBar(0, 25);
+    paintBar(1, 75);
+    paintBar(2, 100);
 };
 
-input.addEventListener('change', measureStrength);
+setInterval(() => measureStrength(), 100);
