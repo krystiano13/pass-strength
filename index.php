0d9d4d72d60051d4910336af6945aa5f2455fa0b
krystiano13 <crystianotv@gmail.com>
2023-08-31 10:30:20 +0200

    add basic styles

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Password Strength</title>
+    <link rel="stylesheet" href="./styles/index.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet">
 </head>
 <body>
     <?php echo "Hello"; ?>
